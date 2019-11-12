bec09f5ea5c4ada3a0bbf990b81afa225d1f2d91
Cédric Menzi <cmenzi@crip.ch>
2019-11-12 20:06:44 +0100

    github issue reproduce

---
+++ Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace MyNamespace
+{
+    using System.Text.RegularExpressions;
+
+    internal class Constants
+    {
+        public static readonly Regex UnhashedNameIdRegex = new Regex(@"^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}_[a-zA-z0-9_\-\.]+_.+$", RegexOptions.Compiled);
+    }
+}
---
+++ NameIdentifier.cs
@@ -0,0 +1,42 @@
+﻿namespace MyNamespace
+{
+    using System;
+    using System.Globalization;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public static class NameIdentifier
+    {
+        public static string Calculate(Guid agentId, string deviceId, string name)
+        {
+            return Calculate($"{agentId}_{deviceId}_{name}");
+        }
+
+        public static string Calculate(string unhashedNameId)
+        {
+            if (string.IsNullOrWhiteSpace(unhashedNameId))
+            {
+                throw new ArgumentNullException(nameof(unhashedNameId), $"{nameof(unhashedNameId)} must not be null, empty or whitespace.");
+            }
+
+            if (!Constants.UnhashedNameIdRegex.IsMatch(unhashedNameId))
+            {
+                throw new ArgumentException($"{nameof(unhashedNameId)} does not match '{Constants.UnhashedNameIdRegex}'.", nameof(unhashedNameId));
+            }
+
+            using (var sha = new SHA256Managed())
+            {
+                byte[] textData = Encoding.UTF8.GetBytes(unhashedNameId);
+                byte[] crypto = sha.ComputeHash(textData);
+
+                var nameId = new StringBuilder();
+                foreach (byte hash in crypto)
+                {
+                    nameId.Append(hash.ToString("x2", CultureInfo.InvariantCulture));
+                }
+
+                return nameId.ToString();
+            }
+        }
+    }
+}
